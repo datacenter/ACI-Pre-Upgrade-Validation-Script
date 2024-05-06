2293146c099b0db854f4bb27bcff055a50f9581c
GM <gmonroy@cisco.com>
2024-05-06 09:12:16 -0400

    Update aci-preupgrade-validation-script.py
    remove .keys() usage for py2
    
    Co-authored-by: takishida <38262981+takishida@users.noreply.github.com>

--- aci-preupgrade-validation-script.py
+++ aci-preupgrade-validation-script.py
@@ -617,7 +617,7 @@ def icurl(apitype, query):
     response = subprocess.check_output(cmd)
     logging.debug('response: ' + str(response))
     imdata = json.loads(response)['imdata']
-    if imdata and "error" in imdata[0].keys():
+    if imdata and "error" in imdata[0]:
         if "not found in class" in imdata[0]['error']['attributes']['text']:
             raise OldVerPropNotFound('cversion does not have requested property')
         else:
