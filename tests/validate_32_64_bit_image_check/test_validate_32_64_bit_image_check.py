e84f922749cd9d31d8430e29cf4c16a916edb61d
Gabriel <gmonroy@cisco.com>
2024-08-12 15:27:56 -0400

    manual test + space convert

--- tests/validate_32_64_bit_image_check/test_validate_32_64_bit_image_check.py
+++ tests/validate_32_64_bit_image_check/test_validate_32_64_bit_image_check.py
@@ -18,36 +18,39 @@ firmware_52_api =	'firmwareFirmware.json'
 firmware_52_api +=	'?query-target-filter=eq(firmwareFirmware.fullVersion,"n9000-15.2(7d)")'
 
 @pytest.mark.parametrize(
-	"icurl_outputs, tversion, expected_result",
-	[
-		##FAILING = AFFECTED VERSION + AFFECTED MO
-		(
-			{firmware_60_api: read_data(dir, "firmwareFirmware_pos.json"), 
-			},
-			"6.0(3e)", script.FAIL_UF,
-		),
-		## FAILING = AFFECTED VERSION + AFFECTED MO NON EXISTING
-		(
-			{firmware_60_api: read_data(dir, "firmwareFirmware_empty.json"),
-			 },
-			 "6.0(3e)", script.FAIL_UF,
-		),
-		##PASSING = AFFECTED VERSION + NON-AFFECTED MO
-		(
-			{firmware_60_api: read_data(dir, "firmwareFirmware_neg.json"),
-			},
-			"6.0(3e)", script.PASS,
-		),
-		##PASSING = NON-AFFECTED VERSION + AFFECTED MO
-		(
-			{firmware_52_api: read_data(dir, "firmwareFirmware_empty.json"),
-			},
-			"5.2(7d)", script.NA,
-		),
+    "icurl_outputs, tversion, expected_result",
+    [
+        ##FAILING = AFFECTED VERSION + AFFECTED MO
+        (
+            {firmware_60_api: read_data(dir, "firmwareFirmware_pos.json"), 
+            },
+            "6.0(3e)", script.FAIL_UF,
+        ),
+        ## FAILING = AFFECTED VERSION + AFFECTED MO NON EXISTING
+        (
+            {firmware_60_api: read_data(dir, "firmwareFirmware_empty.json"),
+             },
+             "6.0(3e)", script.FAIL_UF,
+        ),
+        ##PASSING = AFFECTED VERSION + NON-AFFECTED MO
+        (
+            {firmware_60_api: read_data(dir, "firmwareFirmware_neg.json"),
+            },
+            "6.0(3e)", script.PASS,
+        ),
+        ##PASSING = NON-AFFECTED VERSION + AFFECTED MO
+        (
+            {firmware_52_api: read_data(dir, "firmwareFirmware_empty.json"),
+            },
+            "5.2(7d)", script.NA,
+        ),
 
-	],
+    ],
 )
 def test_logic(mock_icurl, tversion, expected_result):
-	result = script.validate_32_64_bit_image_check(1, 1, script.AciVersion(tversion))
-	assert result == expected_result
-	
+    result = script.validate_32_64_bit_image_check(1, 1, script.AciVersion(tversion))
+    assert result == expected_result
+
+def test_manual():
+    result = script.validate_32_64_bit_image_check(1, 1, None)
+    assert result == script.MANUAL
