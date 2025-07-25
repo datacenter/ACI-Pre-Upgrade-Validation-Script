5714c1f528b0736f03e4bbefafd2cd3787c352e1
Gabriel <gmonroy@cisco.com>
2025-07-25 13:32:16 -0400

    remove echo test

--- tests/test_run_cmd.py
+++ tests/test_run_cmd.py
@@ -5,20 +5,6 @@ script = importlib.import_module("aci-preupgrade-validation-script")
 
 
 @pytest.mark.parametrize(
-    "cmd, splitlines, expected_output",
-    [
-        (["echo", "$'hello\nworld'"], True, ["hello", "world"]),
-        ("echo $'hello\nworld'", True, ["hello", "world"]),
-        (["echo", "$'hello\nworld'"], False, "hello\nworld\n"),
-        ("echo $'hello\nworld'", False, "hello\nworld\n"),
-    ],
-)
-def test_run_cmds(cmd, splitlines, expected_output):
-    result = script.run_cmd(cmd, splitlines)
-    assert result == expected_output
-
-
-@pytest.mark.parametrize(
     "cmd, splitlines, expected_type",
     [
         (["ls", "-l"], True, list),
