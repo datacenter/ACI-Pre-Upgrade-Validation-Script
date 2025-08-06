e90872a17fa843d11dfa29ae456e3ac08ece86fc
Enrique Estrada <jeestrad@cisco.com>
2025-08-06 15:26:45 -0600

    added changes

--- tests/l3out_mtu_check/test_l3out_mtu_check.py
+++ tests/l3out_mtu_check/test_l3out_mtu_check.py
@@ -12,16 +12,24 @@ dir = os.path.dirname(os.path.abspath(__file__))
 
 # icurl queries
 response_json = 'l3extRsPathL3OutAtt.json'
-response_json_virtual = 'l3extVirtualLIfP.json'
+virtual_l3out_api = 'l3extVirtualLIfP.json'
 l2Pols = 'uni/fabric/l2pol-default.json'
 
 @pytest.mark.parametrize(
     "icurl_outputs, expected_result",
     [
-        # No Response, result is NA
-        ({response_json: [], l2Pols: []}, script.NA),
-        # Overlap across different nodes
-        ({response_json: read_data(dir, "l3extRsPathL3OutAtt.json"), l2Pols: read_data(dir, "l2pol-default.json")}, script.MANUAL),
+        # No Response from neither  l3extRsPathL3OutAtt or l3extVirtualLIfP, result is NA
+        ({response_json: [], l2Pols: [], virtual_l3out_api:[]}, script.NA),
+        # No Response from l3extVirtualLIfP, Reponse from l3extRsPathL3OutAtt result is MANUAL
+        ({response_json: read_data(dir, "l3extRsPathL3OutAtt.json"), l2Pols: read_data(dir, "l2pol-default.json"),
+          virtual_l3out_api: []}, script.MANUAL),
+        # No Response from l3extRsPathL3OutAtt, Reponse from l3extVirtualLIfP result is MANUAL
+        ({response_json: [], l2Pols: read_data(dir, "l2pol-default.json"),
+          virtual_l3out_api: read_data(dir, "l3extVirtualLIfP.json")}, script.MANUAL),
+        # Response from l3extRsPathL3OutAtt and l3extVirtualLIfP result is MANUAL
+        ({response_json: read_data(dir, "l3extRsPathL3OutAtt.json"), 
+          l2Pols: read_data(dir, "l2pol-default.json"),
+          virtual_l3out_api: read_data(dir, "l3extVirtualLIfP.json")}, script.MANUAL),        
         # Overlap within the same L3Out - Router ID as loopback (non-VPC)
         # ({response_json: read_data(dir, "same_l3out_rtrId_non_vpc.json")}, script.FAIL_O),
         # # Overlap within the same L3Out - Router ID as loopback (VPC)
