cff063884dc55628b4e74c43e5e48646d69ffaeb
sanjanch <sanjanch@cisco.com>
2026-01-12 15:01:29 +0000

    modified the method to check only the 'raised' faults

--- aci-preupgrade-validation-script.py
+++ aci-preupgrade-validation-script.py
@@ -2991,27 +2991,30 @@ def apic_disk_space_faults_check(cversion, **kwargs):
     doc_url = "https://datacenter.github.io/ACI-Pre-Upgrade-Validation-Script/validations/#apic-disk-space-usage"
     recommended_action = {
         '/firmware': 'Remove unneeded images',
-        '/techsupport': 'Remove unneeded techsupports/cores'
+        '/techsupport': 'Remove unneeded techsupports/cores',
+        '/data/log': 'Remove unneeded logs in var/log/dme/log'
     }
     default_action = 'Contact Cisco TAC.'
     if cversion.same_as('4.0(1h)') or cversion.older_than('3.2(6i)'):
         default_action += ' A typical issue is CSCvn13119.'
 
     dn_regex = node_regex + r'/.+p-\[(?P<mountpoint>.+)\]-f'
-    desc_regex = r'is (?P<usage>\d{2}%) full'
-
+    desc_regex = r'is (?P<usage>\d{2,3}%) full'
+    
     faultInsts = icurl('class',
                        'faultInst.json?query-target-filter=or(eq(faultInst.code,"F1527"),eq(faultInst.code,"F1528"),eq(faultInst.code,"F1529"))')
     for faultInst in faultInsts:
-        fc = faultInst['faultInst']['attributes']['code']
-        dn = re.search(dn_regex, faultInst['faultInst']['attributes']['dn'])
-        desc = re.search(desc_regex, faultInst['faultInst']['attributes']['descr'])
-        if dn and desc:
-            data.append([fc, dn.group('pod'), dn.group('node'), dn.group('mountpoint'),
-                         desc.group('usage'),
-                         recommended_action.get(dn.group('mountpoint'), default_action)])
-        else:
-            unformatted_data.append([fc, faultInst['faultInst']['attributes']['dn'], default_action])
+        lc = faultInst['faultInst']['attributes']['lc']
+        if lc == "raised":
+            fc = faultInst['faultInst']['attributes']['code']
+            dn = re.search(dn_regex, faultInst['faultInst']['attributes']['dn'])
+            desc = re.search(desc_regex, faultInst['faultInst']['attributes']['descr'])
+            if dn and desc:
+                data.append([fc, dn.group('pod'), dn.group('node'), dn.group('mountpoint'),
+                            desc.group('usage'),
+                            recommended_action.get(dn.group('mountpoint'), default_action)])
+            else:
+                unformatted_data.append([fc, faultInst['faultInst']['attributes']['dn'], default_action])
     if not data and not unformatted_data:
         result = PASS
     return Result(
