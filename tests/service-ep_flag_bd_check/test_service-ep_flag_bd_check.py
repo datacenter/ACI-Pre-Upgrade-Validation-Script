de43b4cc681ee6084bc4370b4f76e8dd6f4fe6f6
Enrique Estrada <jeestrad@cisco.com>
2026-01-12 15:23:42 -0600

    issue NewValidation: CSCwi17652 check for service-ep flag Fixes #294 fixed

---
+++ tests/service-ep_flag_bd_check/test_service-ep_flag_bd_check.py
@@ -0,0 +1,76 @@
+import os
+import pytest
+import logging
+import importlib
+from helpers.utils import read_data
+
+script = importlib.import_module("aci-preupgrade-validation-script")
+
+log = logging.getLogger(__name__)
+dir = os.path.dirname(os.path.abspath(__file__))
+
+
+# icurl queries
+vnsLIfCtx_api = "vnsLIfCtx.json"
+vnsLIfCtx_api += "?query-target=self&rsp-subtree=children"
+
+
+@pytest.mark.parametrize(
+    "icurl_outputs, cversion, tversion, expected_result",
+    [
+        # tversion missing
+        (
+            {vnsLIfCtx_api: read_data(dir, "vnsLIfCtx-na.json")},
+            "5.2(8h)",
+            None,
+            script.MANUAL
+        ),
+        # Version not affected (both new)
+        (
+            {vnsLIfCtx_api: read_data(dir, "vnsLIfCtx-pos.json")},
+            "6.0(8h)",
+            "6.1(1g)",
+            script.NA,
+        ),
+        # Version not affected (both old)
+        (
+            {vnsLIfCtx_api: read_data(dir, "vnsLIfCtx-pos.json")},
+            "4.2(7s)",
+            "5.2(4c)",
+            script.NA,
+        ),
+        # Version affected with L4L7 Interface connector without PBR
+        (
+            {vnsLIfCtx_api: read_data(dir, "vnsLIfCtx-pos.json")},
+            "5.2(8h)",
+            "6.0(8e)",
+            script.FAIL_O
+        ),
+        # Version affected with L4L7 Interface connector without PBR
+        (
+            {vnsLIfCtx_api: read_data(dir, "vnsLIfCtx-pos.json")},
+            "5.2(8h)",
+            "6.1(1f)",
+            script.FAIL_O
+        ),
+        # Version affected with L4L7 Interface connector without PBR
+        (
+            {vnsLIfCtx_api: read_data(dir, "vnsLIfCtx-neg.json")},
+            "5.2(8h)",
+            "6.0(8e)",
+            script.PASS
+        ),
+        # Version affected with L4L7 Interface connector without PBR
+        (
+            {vnsLIfCtx_api: read_data(dir, "vnsLIfCtx-neg.json")},
+            "5.2(8h)",
+            "6.1(1f)",
+            script.PASS
+        ),
+    ],
+)
+def test_logic(mock_icurl, cversion, tversion, expected_result):
+    cver = script.AciVersion(cversion)
+    tver = script.AciVersion(tversion) if tversion else None
+    result = script.service_ep_flag_bd_check(1, 1, cver, tver)
+    assert result == expected_result
