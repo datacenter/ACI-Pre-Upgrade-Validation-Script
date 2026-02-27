e4defbfd28919d59fb799d1ebb8be93b91810962
Gabriel <gmonroy@cisco.com>
2026-02-27 13:45:22 -0500

    print cleanup

--- aci-preupgrade-validation-script.py
+++ aci-preupgrade-validation-script.py
@@ -4738,7 +4738,6 @@ def fabricPathEp_target_check(**kwargs):
             fex_a = groups.get("fexA")
             fex_b = groups.get("fexB")
             path = groups.get("path")
-            print(path)
 
             # CHECK FEX ID(s) of extpath(s) is 101 or greater
             if fex_a:
