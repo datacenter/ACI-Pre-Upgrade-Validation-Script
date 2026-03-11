5f1e5c8061872fb50f2146fb7322c0f9b4a766b8
veesenth <veesenth@cisco.com>
2026-03-11 08:40:46 +0000

    refactor: removed unwanted spaces

--- tests/checks/svccore_excessive_class_entries_check/svccore_excessive_class_entries_check.py
+++ tests/checks/svccore_excessive_class_entries_check/svccore_excessive_class_entries_check.py
@@ -5,12 +5,9 @@ import importlib
 from helpers.utils import read_data
 
 script = importlib.import_module("aci-preupgrade-validation-script")
-
 log = logging.getLogger(__name__)
 dir = os.path.dirname(os.path.abspath(__file__))
 test_function = "svccore_excessive_data_check"
-
-# icurl queries
 svccoreClassEntry = 'svccoreCtrlr.json?&rsp-subtree-include=count'
 svccoreNodeEntry = 'svccoreNode.json?&rsp-subtree-include=count'
 
@@ -19,32 +16,20 @@ svccoreNodeEntry = 'svccoreNode.json?&rsp-subtree-include=count'
     [
         # No excessive class entries
         (
-            {
-                svccoreClassEntry: read_data(dir, "svccore_positive.json"),
-                svccoreNodeEntry: read_data(dir, "svccoreNode_positive.json")
-            },
+            {svccoreClassEntry: read_data(dir, "svccore_positive.json"),svccoreNodeEntry: read_data(dir, "svccoreNode_positive.json")},
             script.PASS,
         ),
         # Excessive class entries found
         (
-            {
-                svccoreClassEntry: read_data(dir, "svccore_negative.json"),
-                svccoreNodeEntry: read_data(dir, "svccoreNode_positive.json")
-            },
+            {svccoreClassEntry: read_data(dir, "svccore_negative.json"),svccoreNodeEntry: read_data(dir, "svccoreNode_positive.json")},
             script.MANUAL,
         ),
         (
-            {
-                svccoreClassEntry: read_data(dir, "svccoreNode_negative.json"),
-                svccoreNodeEntry: read_data(dir, "svccoreNode_negative.json")
-            },
+            {svccoreClassEntry: read_data(dir, "svccoreNode_negative.json"),svccoreNodeEntry: read_data(dir, "svccoreNode_negative.json")},
             script.MANUAL,
         ),
         (
-            {
-                svccoreClassEntry: read_data(dir, "svccore_positive.json"),
-                svccoreNodeEntry: read_data(dir, "svccoreNode_negative.json")
-            },
+            {svccoreClassEntry: read_data(dir, "svccore_positive.json"),svccoreNodeEntry: read_data(dir, "svccoreNode_negative.json")},
             script.MANUAL,
         )
     ],
@@ -53,4 +38,3 @@ svccoreNodeEntry = 'svccoreNode.json?&rsp-subtree-include=count'
 def test_logic(run_check,mock_icurl,expected_result):
     result = run_check()
     assert result.result == expected_result
-    
